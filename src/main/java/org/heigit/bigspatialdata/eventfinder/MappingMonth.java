697da888946572a94f95f7b4871b697014ed1cd4
yg2 <yair.grinberger@uni-heidelberg.de>
2019-03-25 14:19:19 +0000

    Add variable for recording the number of geometry and tag actions; Add function for pairwise summing of two arrays (perhaps this could be done more efficiently within the lambda operation in MonthCombiner?)

--- src/main/java/org/heigit/bigspatialdata/eventfinder/MappingMonth.java
+++ src/main/java/org/heigit/bigspatialdata/eventfinder/MappingMonth.java
@@ -8,12 +8,14 @@ public class MappingMonth {
   private Integer contributions; // total number of contributions during t
   private HashMap<ContributionType, Integer> types_count; // number of contributions by each type
   private HashMap<Integer, Integer> user_counts; // number of active users during t
+  private HashMap<Long, int[]> entity_edits;
 
   public MappingMonth(Integer contributions, HashMap<Integer, Integer> user_counts,
-      HashMap<ContributionType, Integer> types_count) {
+      HashMap<ContributionType, Integer> types_count, HashMap<Long, int[]> entity_edits) {
     this.user_counts = user_counts;
     this.contributions = contributions;
     this.types_count = types_count;
+    this.entity_edits = entity_edits;
   }
 
   public HashMap<Integer, Integer> getUser_counts() {
@@ -39,5 +41,16 @@ public class MappingMonth {
   public void set_types_count(HashMap<ContributionType, Integer> types_count) {
     this.types_count = types_count;
   }
-
+  
+  public void set_entity_edits(HashMap<Long, int[]> entity_edits) {
+	  this.entity_edits = entity_edits;
+  }
+  
+  public int[] pairwise_sum(int[] a, int[] b) {
+	  int[] sum = new int[a.length];
+	  for (int i=0; i<a.length; i++) {
+		  sum[i] = a[i] + b[i];
+	  }
+	  return sum;
+  }
 }
