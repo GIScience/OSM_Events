75ea10bf21535573cad37f710ed8340a4e91e829
yg2 <yair.grinberger@uni-heidelberg.de>
2019-04-11 20:23:52 +0000

    Add consideration of the pre-October 2007 issue: the data series starts from the 2007-10, the t values (x values) of the time series do not start from zero, but according to the temporal gap between the first contribution and October 2007; Create file for recording convergence issues

--- src/main/java/org/heigit/bigspatialdata/eventfinder/EventFinder.java
+++ src/main/java/org/heigit/bigspatialdata/eventfinder/EventFinder.java
@@ -10,6 +10,7 @@ import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -152,7 +153,10 @@ public class EventFinder {
     //devide result into resulty per geometry
     SortedMap<Integer, SortedMap<OSHDBTimestamp, MappingMonth>> nest
         = OSHDBCombinedIndex.nest(months);
-
+    
+    File conv_file = new File("target/Convergence_errors.csv");
+    FileWriter conv_writer = new FileWriter(conv_file);
+    conv_writer.write("GeomNr.\n")
     //TODO many loops following. Can we simplify?
     //iterate
     nest.forEach((Integer geom, SortedMap<OSHDBTimestamp, MappingMonth> geomContributions) -> {
@@ -185,22 +189,35 @@ public class EventFinder {
       }
 
       // create data for curve fitting
+      SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+      Date date2007 = null;
+      try {
+          date2007 = dateFormat.parse("2007-09-30");
+      } catch (ParseException e) {
+          e.printStackTrace();
+      }
       ArrayList<WeightedObservedPoint> points = new ArrayList<WeightedObservedPoint>();
       int i = 0;
-      Iterator<Integer> values = acc_result.values().iterator();
+      Iterator<OSHDBTimestamp> values = acc_result.keySet().iterator();
       while (values.hasNext()) {
-        float v = (float) values.next();
-        WeightedObservedPoint point = new WeightedObservedPoint(1.0, i, v);
-        points.add(point);
+        OSHDBTimestamp d = values.next();
+    	float v = acc_result.get(d);
+    	Date date = d.toDate();
+    	Boolean aft = date.after(date2007);
+    	if (aft) {
+    		WeightedObservedPoint point = new WeightedObservedPoint(1.0, i, v);
+    		points.add(point);
+    	}
         i++;
       }
-
+          
       // fit curve
       MyFuncFitter fitter = new MyFuncFitter();
       double[] coeffs;
       try {
         coeffs = fitter.fit(points);
       } catch (ConvergenceException ex) {
+        conv_writer.write(geom.toString()+"\n");
         ex.printStackTrace();
         return;
       }
@@ -276,7 +293,8 @@ public class EventFinder {
       }
       out.put(geom, list); // add to list of events
     });
-
+    
+    conv_writer.close()
     createStarted.stop();
     double toMinutes = (createStarted.getTime() / 1000.0) / 60.0;
 
