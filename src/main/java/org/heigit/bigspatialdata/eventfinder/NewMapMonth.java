b4d776a80478c03fd705ed731ed107cfa9ed1dfc
yg2 <yair.grinberger@uni-heidelberg.de>
2019-04-16 13:33:50 +0000

    Adapt to using EditCountEnum

--- src/main/java/org/heigit/bigspatialdata/eventfinder/NewMapMonth.java
+++ src/main/java/org/heigit/bigspatialdata/eventfinder/NewMapMonth.java
@@ -9,7 +9,7 @@ public class NewMapMonth implements SerializableSupplier<MappingMonth> {
   @Override
   public MappingMonth get() {
     return new MappingMonth(0, new HashMap<Integer, Integer>(),
-        new HashMap<ContributionType, Integer>());
+        new HashMap<ContributionType, Integer>(), new EditCountEnum(0, 0));
   }
 
 }
