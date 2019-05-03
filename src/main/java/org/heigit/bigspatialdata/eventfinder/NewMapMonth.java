7201c6821664eb2cd632dc4f9ee68eaf7fe703f5
yg2 <yair.grinberger@uni-heidelberg.de>
2019-05-03 15:01:45 +0000

    Update to fit reading data from file - add zero inputs for number of mappers and max contribution in the creation of a new mapping months

--- src/main/java/org/heigit/bigspatialdata/eventfinder/NewMapMonth.java
+++ src/main/java/org/heigit/bigspatialdata/eventfinder/NewMapMonth.java
@@ -9,7 +9,8 @@ public class NewMapMonth implements SerializableSupplier<MappingMonth> {
   @Override
   public MappingMonth get() {
     return new MappingMonth(0, new HashMap<Integer, Integer>(),
-        new HashMap<ContributionType, Integer>(), new EditCountEnum(0, 0));
+        new HashMap<ContributionType, Integer>(), new EditCountEnum(0, 0), 
+        0, 0);
   }
 
 }
